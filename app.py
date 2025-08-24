5f8d99d512dc34f8cf882f646f63b404a622c3f1
SouzaEdgar <edgarss2013@gmail.com>
2025-08-23 21:22:59 -0300

    refactor: app apenas inicializa | url_service trabalha com as urls e parametros

--- app.py
+++ app.py
@@ -1,51 +1,11 @@
-from flask import Flask, render_template, request
-import functions as adops
-import re
+from flask import Flask
+from routes.main_routes import main_bp
 
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    resultados = []
-
-    if request.method == "POST":
-        # ===== URLs ===== 
-        user_urls = request.form.get("url", "").splitlines()
-        urls = []
-        for linha in user_urls:
-            linha = linha.strip()
-            if linha: # <--- ignorar linhas vazias
-                urls.append(linha)
-
-        # ===== Parametros ===== 
-        raw_params = request.form.get("parameter", "")
-        user_params = re.split(r"[;\n,]+", raw_params)
-        params = []
-        for param in user_params:
-            param = param.strip()
-            if param:
-                params.append(param)
-        
-        # ===== Trabalhar com cada URL ===== 
-        for i, url in enumerate(urls, start=1):
-            resposta = adops.get_response(url)
-            if adops.verify_response(resposta):
-                print(i, resposta)
-                url_final = adops.redirects_history(resposta)
-                parametros = adops.parameters_search(resposta, params)
-                print(parametros)
-                # passar os parametros para string
-                parametros_str = f"<ul>{"".join([f"<li><b>{k}:</b> {v}</li>" for k, v in parametros])}</ul>"
-                status_final = adops.status_code(resposta)
-
-                resultados.append({
-                    "position" : i,
-                    "url": url_final[-1],
-                    "params": parametros_str,
-                    "status": status_final[-1]
-                })
-
-    return render_template("index.html", resultados=resultados)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main_bp) # Registra rotas
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
--- services/url_service.py
+++ services/url_service.py
@@ -9,6 +9,7 @@ def process_urls(urls_raw: list, params_raw: str):
     for u in urls_raw:
         if u.strip():
             urls.append(u.strip())
+
     # ===== Trata - Parametros ===== 
     user_params = re.split(r"[;\n,]+", params_raw)
     params = []
@@ -16,4 +17,20 @@ def process_urls(urls_raw: list, params_raw: str):
         if p.strip():
             params.append(p.strip())
 
-            
+    # ===== Trabalhar com cada URL =====
+    for i, url in enumerate(urls, start=1):
+        resposta = adops.get_response(url)
+
+        if adops.verify_response(resposta):
+            url_final = resposta.url
+            parametros = adops.parameters_search(url_final, params)
+            parametros_str = f"<ul>{"".join([f"<li><b>{k}:</b> {v}</li>" for k, v in parametros])}</ul>"
+
+            resultados.append({
+                "position": i,
+                "url": url_final,
+                "params": parametros_str,
+                "status": resposta.status_code
+            })
+
+    return resultados
